392513f894413f4fc1a774bb24e0a7b954cae4f3
Tim Robillard <robillt@algaonquincollege.com>
2024-08-08 18:18:44 -0400

    add driver router

--- week12/carAPI/src/index.js
+++ week12/carAPI/src/index.js
@@ -41,6 +41,7 @@ app.get('/', (_req, res) => {
 
 app.use('/auth', authRouter);
 app.use('/api/cars', carsRouter);
+app.use('/api/drivers', driversRouter);
 
 app.get('/login-success', (req, res) => {
   res.send(`Your token is ${req.query.token}`);
